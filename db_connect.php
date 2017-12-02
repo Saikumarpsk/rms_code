d3675879a13079b5037037978fffd69ee75aaafc
P saikumar <saikumar.patnaikuni@kelltontech.com>
2017-12-02 23:14:20 +0530

    changes for code

--- db_connect.php
+++ db_connect.php
@@ -1,19 +1,13 @@
 <?php
 
 	
-		 if (!$link = mysql_connect('localhost', 'root', 'K@#L&*!)%')) 
-		    {
-			echo 'Could not connect to mysql';
-			exit;
-		    }
-	
-		 /*if (!$link = mysql_connect('localhost', 'root', 'root123')) 
-		    {
-			echo 'Could not connect to mysql';
-			exit;
-		    }*/
-	
-	
+	 if (!$link = mysql_connect('localhost', 'root', 'K@#L&*!)%')) 
+	    {
+		echo 'Could not connect to mysql';
+		exit;
+	    }
+
+		
    
 	   
 	    if (!mysql_select_db('demo_suneel', $link)) {
