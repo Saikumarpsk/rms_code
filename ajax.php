af5d9f3085b6dc12d7989a4f21d27c8696e75447
P saikumar <saikumar.patnaikuni@kelltontech.com>
2017-12-03 16:57:31 +0530

    changes for issue

--- ajax.php
+++ ajax.php
@@ -50,78 +50,10 @@
 	}
 
 	if ($_SERVER['PATH_INFO'] === "/fields") {
-		if(isset($_POST)  && $_POST['condition_type'] == 2)
-    {
-        $cust_id=$_POST['cust_id'];
-        $countries =$_POST['countries'];
-
-        $array=explode(',',$countries);
-
-        $unnecessary =array(1,'on','on');
-
-        $result = array_diff($array,$unnecessary);
-
-            //$contries =implode(',', $result);
-
-        $contries="'" . implode ( "', '", $result ) . "'";
-
-
-
-        $sql =  "select * from  asset_id_list where country_id in($contries) and customer_id = '$cust_id' ";
-        $result = mysql_query($sql, $link);
-
-        while($val1 = mysql_fetch_array($result))
-        {
-                echo "<div>
-                         <input type='checkbox' name=checkbox class='checkbox2'   value='".$val1["field_id"] ." '>
-                         <label for='checkbox1'>" .$val1["field_id"] ." </label>
-
-                </div>";
-
-        }
-
-    }
-
+		echo 'hello';die();
 	}
 
-	if ($_SERVER['PATH_INFO'] === "/maplist") {
-		
-	if(isset($_POST)  && $_POST['condition_type'] == 3)
-    {
-        $cust_id=$_POST['cust_id'];
-        $countries =$_POST['fields'];
-
-        $array=explode(',',$countries);
-
-        $unnecessary =array(1,'on','on');
-
-        $result = array_diff($array,$unnecessary);
-
-       //$contries =implode(',', $result);
-
-        $fields ="'" . implode ( "', '", $result ) . "'"; 
-
-        $sql =  "select * from  asset_id_list where field_id in($fields) and customer_id = '$cust_id' ";
-        $result = mysql_query($sql, $link);
-        $lat = [];
-        $long = [];
-        while($val1 = mysql_fetch_array($result))
-        {
-                
-                $lat = $val1["asset_loc_lat"];
-                $long = $val1["asset_loc_long"];
-               
-                echo "<input type='hidden' name=checkbox".$val1["asset_id"]." class='checkbox2'   value='".$val1["asset_id"] ." ' lat='".$val1["asset_loc_lat"]."' long='".$val1["asset_loc_long"]."' country_name='".$val1["country_id"]."' asset_name='".$val1["asset_name"]."'>
-			<ul class='sidebar-menu' data-widget='tree'>  
-                         <li> <a href='#'><i class='fa fa-circle-o text-green'></i><span>" .$val1["asset_name"] ."</span></a> </li>
-                     </ul>";
-
-        }
-
-    }
-
-	}
-
+	
 ?>
 		
 			
