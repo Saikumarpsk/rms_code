b4c38600e8d3d487c3aee65abb88dd5f506d8bc7
P saikumar <saikumar.patnaikuni@kelltontech.com>
2017-12-06 12:02:04 +0530

    changes for issues

--- db_connect.php
+++ db_connect.php
@@ -1,30 +1,14 @@
 <?php
-            $server_name = $_SERVER['SERVER_NAME'];
-            
-            if($server_name == 'localhost'){
-                
-                if (!$link = mysql_connect('localhost', 'root', 'root123')) 
-                {
-                    echo 'Could not connect to mysql';
-                    exit;
-                }
 
-                if (!mysql_select_db('demo_suneel1', $link)) {
-                    echo 'Could not select database';
-                    exit;
-                }
-            }else if($server_name == "119.235.51.66"){
-                if (!$link = mysql_connect('localhost', 'root', 'K@#L&*!)%')) 
-                {
-                    echo 'Could not connect to mysql';
-                    exit;
-                }
-
-                if (!mysql_select_db('demo_rmss_new', $link)) {
-                    echo 'Could not select database';
-                    exit;
-                }
-            }
 	
-	    
+	    if (!$link = mysql_connect('localhost', 'root', 'K@#L&*!)%')) 
+	    {
+		echo 'Could not connect to mysql';
+		exit;
+	    }
+
+	    if (!mysql_select_db('demo_rmss_new', $link)) {
+		echo 'Could not select database';
+		exit;
+	    }
 ?>
